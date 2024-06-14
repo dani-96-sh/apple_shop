ebaaffd7b754640036fdd5c398ef51620bb10191
dani <danyalshahangi@gmail.com>
2024-06-14 12:24:11 +0330

    lastcomiit

--- lib/data/source/Athentication.dart
+++ lib/data/source/Athentication.dart
@@ -6,15 +6,15 @@ class AuthenticationRemote {
 
   Future<void> register(
       String username, String password, String passwordConfirm) async {
-    // try {
-    final response = await _dio.post('collections/users/records', data: {
-      'username': username,
-      'password': password,
-      'passwordConfirm': passwordConfirm
-    });
-    print('${response.statusCode}');
-    // } on DioException catch (ex) {
-    //   print(ex.response?.statusCode);
-    // }
+    try {
+      final response = await _dio.post('collections/users/records', data: {
+        'username': username,
+        'password': password,
+        'passwordConfirm': passwordConfirm
+      });
+      print('${response.statusCode}');
+    } on DioException catch (ex) {
+      print(ex.response?.statusCode);
+    }
   }
 }
